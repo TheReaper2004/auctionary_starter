371e37a86bd7f0864041c621c281644668a43a42
TheReaper2004 <121404033+TheReaper2004@users.noreply.github.com>
2025-11-10 00:47:02 +0000

    npm run main

--- app/index.js
+++ app/index.js
@@ -1,12 +1,16 @@
-const express = require("express");
+const express = require("express"); 
 const app = express();
 
+// Define a port for this app to run on
 const port = 3000;
 
-app.get("/", (req, res) => {
+//a simple route to check it works!
+app.get("/", (req, res) => { 
   res.send("Hello, World!");
 });
 
-app.listen(port, () => {
+//make sure the app can be accessed
+app.listen(port, () => {  
   console.log("App is listing on port");
 });
+
