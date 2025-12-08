a130f9c18fa071e3ff1321bea2795b3966efcc63
Kaneki <121404033+TheReaper2004@users.noreply.github.com>
2025-12-08 10:56:29 +0000

    i will do it at home it start to get interesting

---
+++ app/controllers/user.server.controllers.js
@@ -0,0 +1,33 @@
+const users = require("../controllers/user.server.controllers")
+
+module.exports = finction(app){
+    app.route("/users")
+    .post(users.create_account);
+
+    app.route("/login")
+    .post(users.login);
+
+    app.route("/logout")
+    .post(users.logout);
+}
+
+
+const create_account = (req, res) => {
+    return res.sendStatus(500);
+}
+
+const login = (req, res) => {
+    return res.sendStatus(500);
+}
+
+const logout = (req, res) => {
+    return res.sendStatus(500);
+}
+
+
+//this is to acress by other files
+module.exports = {
+    create_account: create_account,
+    login: login,
+    logout: logout,
+}
